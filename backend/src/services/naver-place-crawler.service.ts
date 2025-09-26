01fe0135994f482a77a7af4397a9cf2c612545ea
tjsgh1217 <tjsgh1217@gmail.com>
2025-09-26 20:38:25 +0900

    fix: puppteer-core 크롬 삭제

--- backend/src/services/naver-place-crawler.service.ts
+++ backend/src/services/naver-place-crawler.service.ts
@@ -18,7 +18,7 @@ export class NaverPlaceCrawlerService {
     if (!this.browser) {
       this.browser = await puppeteer.launch({
         headless: true,
-        executablePath: '/usr/bin/google-chrome-stable',
+        // executablePath: '/usr/bin/google-chrome-stable',
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
